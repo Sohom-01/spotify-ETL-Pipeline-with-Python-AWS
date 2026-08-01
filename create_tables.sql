2b98677b9ec7de3622f586b5d445596f6864a331
Sohom Das <91403876+Sohom-01@users.noreply.github.com>
2026-08-01 21:17:46 +0530

    Refactor SQL table definitions for clarity
    Updated table definitions for customer profiles, product inventory, and sales transactions.

--- create_tables.sql
+++ create_tables.sql
@@ -1,8 +1,8 @@
 -- Customer Profiles
 CREATE TABLE customer_profiles
 (
-	customer_id			INTEGER PRIMARY KEY,
-	age					    SMALLINT,
+	customer_id			  INTEGER PRIMARY KEY,
+	age					  SMALLINT,
 	gender				  VARCHAR(20),
 	location			  VARCHAR(50),
 	join_date			  DATE
@@ -13,20 +13,20 @@ CREATE TABLE product_inventory
 (
 	product_id 			INTEGER PRIMARY KEY,
 	product_name		VARCHAR(100),
-	category			  VARCHAR(50),
+	category			VARCHAR(50),
 	stock_level			INTEGER,
-	price				    NUMERIC(10,2)
+	price				NUMERIC(10,2)
 );
 
 -- Sales Transactions
-CREATE TABLE sales_transactions
+CREATE TABLE sales_transaction_staging 
 (
-	transaction_id 		  INTEGER PRIMARY KEY,
-	customer_id 		    INTEGER	REFERENCES customer_profiles(customer_id),
-	product_id 			    INTEGER REFERENCES product_inventory(product_id),
-	quantity_purchased  INTEGER,
-	transaction_date 	  DATE,
-	price 				      NUMERIC(10,2)
+    transaction_id      INTEGER,
+    customer_id         INTEGER,
+    product_id          INTEGER,
+    quantity_purchased  INTEGER,
+    transaction_date    DATE,
+    price               NUMERIC(10, 2)
 );
 
 
